003736a90a78eed24d3f2b954244181c88e9e3a9
Your Name <you@example.com>
2020-04-21 05:31:35 -0700

    Updates

---
+++ precache-manifest.d6ea5d280d3925f0f5eb902a1ead7fee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc07a201551017e29cfdac327575efb",
+    "url": "/19it197/react-in-github-to-do/index.html"
+  },
+  {
+    "revision": "ea798b03793d1146d8be",
+    "url": "/19it197/react-in-github-to-do/static/css/main.68970909.chunk.css"
+  },
+  {
+    "revision": "48de76597e87878bfc24",
+    "url": "/19it197/react-in-github-to-do/static/js/2.fe8c9100.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/19it197/react-in-github-to-do/static/js/2.fe8c9100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea798b03793d1146d8be",
+    "url": "/19it197/react-in-github-to-do/static/js/main.000b5e72.chunk.js"
+  },
+  {
+    "revision": "6283c168715bdb8458b4",
+    "url": "/19it197/react-in-github-to-do/static/js/runtime-main.02d0c2b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/19it197/react-in-github-to-do/static/media/logo.5d5d9eef.svg"
+  }
+]);
